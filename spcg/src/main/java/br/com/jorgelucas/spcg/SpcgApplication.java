9de52d46b18e1cb5da9b77867cb9773b76103335
JorgeAlmeidaDev <jorgelucasrosadealmeida@gmail.com>
2024-10-29 09:58:40 -0300

    Initial commit

---
+++ spcg/src/main/java/br/com/jorgelucas/spcg/SpcgApplication.java
@@ -0,0 +1,13 @@
+package br.com.jorgelucas.spcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpcgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpcgApplication.class, args);
+    }
+
+}
---
+++ spcg/src/test/java/br/com/jorgelucas/spcg/SpcgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jorgelucas.spcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
